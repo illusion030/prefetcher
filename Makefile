de2af359f4d820acb37111815418a3149479972e
illusion030 <illusion030@gmail.com>
2017-03-13 23:35:41 +0800

    Use deploy OOP techniques
    Use deploy OOP techniques to package different practices
    Modify Makefile to generate different executables

--- Makefile
+++ Makefile
@@ -1,13 +1,37 @@
+CC ?= gcc
 CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
+
+EXEC = naive sse sse_prefetch
 
 GIT_HOOKS := .git/hooks/applied
 
-all: $(GIT_HOOKS) main.c
-	$(CC) $(CFLAGS) -o main main.c
+all: $(GIT_HOOKS) $(EXEC)
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
 	@echo
 
+naive: main.c
+	$(CC) $(CFLAGS) -D NAIVE -o naive main.c
+
+sse: main.c
+	$(CC) $(CFLAGS) -D SSE -o sse main.c
+
+sse_prefetch: main.c
+	$(CC) $(CFLAGS) -D SSE_PREFETCH -o sse_prefetch main.c
+
+run: $(EXEC)
+	./naive
+	./sse
+	./sse_prefetch
+
+cache-test: $(EXEC)
+	sudo perf stat --repeat 100 \
+	-e cache-misses,cache-references,L1-dcache-load-misses,L1-icache-load-misses ./naive
+	sudo perf stat --repeat 100 \
+	-e cache-misses,cache-references,L1-dcache-load-misses,L1-icache-load-misses ./sse
+	sudo perf stat --repeat 100 \
+	-e cache-misses,cache-references,L1-dcache-load-misses,L1-icache-load-misses ./sse_prefetch
+
 clean:
-	$(RM) main
+	$(RM) $(EXEC)
--- main.c
+++ main.c
@@ -31,64 +31,81 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 
 int main()
 {
+
+    Object *o = NULL;
+#ifdef NAIVE
+    if(init_object_naive(&o) == -1) {
+        printf("Init object naive fail\n");
+        return -1;
+    }
+#elif SSE
+    if(init_object_sse(&o) == -1) {
+        printf("Init object sse fail\n");
+        return -1;
+    }
+#elif SSE_PREFETCH
+    if(init_object_sse_prefetch(&o) == -1) {
+        printf("Init object sse prefetch fail\n");
+        return -1;
+    }
+#endif
+
     /* verify the result of 4x4 matrix */
-    {
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                           8, 9, 10, 11, 12, 13, 14, 15
-                         };
-        int testout[16];
-        int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                             2, 6, 10, 14, 3, 7, 11, 15
-                           };
+    int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
+                       8, 9, 10, 11, 12, 13, 14, 15
+                     };
+    int testout[16];
+    int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
+                         2, 6, 10, 14, 3, 7, 11, 15
+                       };
 
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testin[y * 4 + x]);
-            printf("\n");
-        }
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++)
+            printf(" %2d", testin[y * 4 + x]);
         printf("\n");
-        sse_transpose(testin, testout, 4, 4);
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testout[y * 4 + x]);
-            printf("\n");
-        }
-        assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
-               "Verification fails");
+    }
+    printf("\n");
+    o->src = testin;
+    o->dst = testout;
+    o->w = 4;
+    o->h = 4;
+    o->transpose(o);
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++)
+            printf(" %2d", testout[y * 4 + x]);
+        printf("\n");
+    }
+    assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
+           "Verification fails");
+
+    /*caculate the spending time in transpose*/
+    struct timespec start, end;
+    int *src, *out;
+    if(!(src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H))) {
+        printf("Malloc src fail\n");
+        return -1;
+    }
+    if(!(out = (int *) malloc(sizeof(int) * TEST_W * TEST_H))) {
+        printf("Malloc out fail\n");
+        return -1;
     }
 
-    {
-        struct timespec start, end;
-        int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    srand(time(NULL));
+    for (int y = 0; y < TEST_H; y++)
+        for (int x = 0; x < TEST_W; x++)
+            *(src + y * TEST_W + x) = rand();
 
-        srand(time(NULL));
-        for (int y = 0; y < TEST_H; y++)
-            for (int x = 0; x < TEST_W; x++)
-                *(src + y * TEST_W + x) = rand();
+    o->src = src;
+    o->dst = out;
+    o->w = TEST_W;
+    o->h = TEST_H;
+    clock_gettime(CLOCK_REALTIME, &start);
+    o->transpose(o);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("%s: \t %ld us\n", o->name, diff_in_us(start, end));
 
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));
-
-        free(src);
-        free(out0);
-        free(out1);
-        free(out2);
-    }
+    free(src);
+    free(out);
 
     return 0;
 }
