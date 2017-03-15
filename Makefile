c85f2b4ab7e64f41913c81ba9783fdc86fce1c2b
illusion030 <illusion030@gmail.com>
2017-03-16 02:15:22 +0800

    Use AVX implement matrix transpose

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC ?= gcc
-CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
+CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra -mavx2
 
-EXEC = naive sse sse_prefetch
+EXEC = naive sse sse_prefetch avx
 
 GIT_HOOKS := .git/hooks/applied
 
@@ -20,18 +20,28 @@ sse: main.c
 sse_prefetch: main.c
 	$(CC) $(CFLAGS) -DSSE_PREFETCH -DNAME=\"sse_prefetch\" -o sse_prefetch main.c
 
+avx: main.c
+	$(CC) $(CFLAGS) -DAVX -DNAME=\"avx\" -o avx main.c
+
 run: $(EXEC)
 	./naive
 	./sse
 	./sse_prefetch
+	./avx
 
 cache-test: $(EXEC)
+	echo 1 | sudo tee /proc/sys/vm/drop_caches
 	perf stat --repeat 100 \
 	-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,r014c,r024c,r81d0,r01d1,r02d1,r04d1,r08d1,r10d1,r20d1 ./naive
+	echo 1 | sudo tee /proc/sys/vm/drop_caches
 	perf stat --repeat 100 \
 	-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,r014c,r024c,r81d0,r01d1,r02d1,r04d1,r08d1,r10d1,r20d1 ./sse
+	echo 1 | sudo tee /proc/sys/vm/drop_caches
 	perf stat --repeat 100 \
 	-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,r014c,r024c,r81d0,r01d1,r02d1,r04d1,r08d1,r10d1,r20d1 ./sse_prefetch
+	echo 1 | sudo tee /proc/sys/vm/drop_caches
+	perf stat --repeat 100 \
+	-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,r014c,r024c,r81d0,r01d1,r02d1,r04d1,r08d1,r10d1,r20d1 ./avx
 
 clean:
 	$(RM) $(EXEC)
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <assert.h>
 
 #include <xmmintrin.h>
+#include <immintrin.h>
 
 #define TEST_W 4096
 #define TEST_H 4096
@@ -39,33 +40,47 @@ int main()
     sse_init(&trans_i);
 #elif SSE_PREFETCH
     sse_prefetch_init(&trans_i);
+#elif AVX
+    avx_init(&trans_i);
 #endif
 
     /* verify the result of 4x4 matrix */
-    int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                       8, 9, 10, 11, 12, 13, 14, 15
+    int testin[64] = { 0, 1,  2,  3,  4,  5,  6,  7,
+                       8, 9, 10, 11, 12, 13, 14, 15,
+                       16, 17, 18, 19, 20, 21, 22, 23,
+                       24, 25, 26, 27, 28, 29, 30, 31,
+                       32, 33, 34, 35, 36, 37, 38, 39,
+                       40, 41, 42, 43, 44, 45, 46, 47,
+                       48, 49, 50, 51, 52, 53, 54, 55,
+                       56, 57, 58, 59, 60, 61, 62, 63
                      };
-    int testout[16];
-    int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                         2, 6, 10, 14, 3, 7, 11, 15
+    int testout[64];
+    int expected[64] = { 0, 8, 16, 24, 32, 40, 48, 56,
+                         1, 9, 17, 25, 33, 41, 49, 57,
+                         2, 10, 18, 26, 34, 42, 50, 58,
+                         3, 11, 19, 27, 35, 43, 51, 59,
+                         4, 12, 20, 28, 36, 44, 52, 60,
+                         5, 13, 21, 29, 37, 45, 53, 61,
+                         6, 14, 22, 30, 38, 46, 54, 62,
+                         7, 15, 23, 31, 39, 47, 55, 63
                        };
 
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++)
-            printf(" %2d", testin[y * 4 + x]);
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++)
+            printf(" %2d", testin[y * 8 + x]);
         printf("\n");
     }
     printf("\n");
 
-    (trans_i.transpose)(testin, testout, 4, 4);
+    (trans_i.transpose)(testin, testout, 8, 8);
 
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++)
-            printf(" %2d", testout[y * 4 + x]);
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++)
+            printf(" %2d", testout[y * 8 + x]);
         printf("\n");
     }
 
-    assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
+    assert(0 == memcmp(testout, expected, 64 * sizeof(int)) &&
            "Verification fails");
 
     /*caculate the spending time in transpose*/
